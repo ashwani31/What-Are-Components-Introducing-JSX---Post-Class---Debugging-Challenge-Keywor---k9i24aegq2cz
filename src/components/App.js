38870f5b692271c2fccabe164011a9a4298c9e57
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-08 13:01:38 +0530

    test 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-
-  let [count, setCount] = useState(0)
+  let [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+      <button class="increment-button" onClick={increment}>
+        Increment
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
